6ec8ad832193188eef5fc6b4b8866098414e037f
ivan babida <babidaivan09@gmail.com>
2025-05-10 12:37:37 +0800

    added an add to cart function on /productId page

--- app/components/nav/Nav.tsx
+++ app/components/nav/Nav.tsx
@@ -4,9 +4,6 @@ import { FaOpencart } from "react-icons/fa6";
 import { useState, useEffect } from "react";
 import Image from 'next/image';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-// import { Button } from "@/components/ui/button"
-// import { Input } from "@/components/ui/input"
-// import { Label } from "@/components/ui/label"
 import {
     Popover,
     PopoverContent,
--- app/shop/products/QuantityButton.tsx
+++ app/shop/products/QuantityButton.tsx
@@ -1,11 +1,30 @@
 'use client'
 
+import { Product } from "@prisma/client";
 import { useState } from "react"
 import { FaOpencart } from "react-icons/fa6";
 
 export default function QuantityButton({ product }: { product: any }) {
 
     const [quantity, setQuantity] = useState(1);
+
+    const addToCart = async ({productId, quantity}: {productId: string, quantity: number}) => {
+
+        try {
+            const res = await fetch('/api/cart', {
+                method: 'POST',
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify({productId, quantity})
+            })
+
+            if (!res.ok) {
+                console.log('unsuccessful')
+            }
+        } catch (error) {
+            console.error("An unexpected error occurred haha:", error);
+        }
+
+    }
 
     return (
 
@@ -28,7 +47,7 @@ export default function QuantityButton({ product }: { product: any }) {
             <div className="flex gap-2 mb-[2.6rem]">
                 <button className="p-1 w-[60%] text-white bg-main">Buy</button>
 
-                <button className="p-1 w-[40%] border-2 text-subMain border-subMain flex items-center justify-center">
+                <button onClick={() => addToCart({ productId: product.product_id, quantity })}  className="p-1 w-[40%] border-2 text-subMain border-subMain flex items-center justify-center">
                     <FaOpencart className="text-[2rem]" />
                 </button>
             </div>
