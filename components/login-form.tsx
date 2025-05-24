133151d088bb14acf8c5cc0ec0a742d1e3e624a6
ivan babida <babidaivan09@gmail.com>
2025-05-24 20:57:38 +0800

    initialized admin dashboard, finished admin login page and functionality

---
+++ components/login-form.tsx
@@ -0,0 +1,95 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+
+export function LoginForm({ className, ...props }: React.ComponentProps<"div">) {
+
+  const router = useRouter()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    setError("")
+
+    try {
+      const res = await fetch("/api/admin/auth/login/", {
+        method: 'POST',
+        headers: {'Content-type':'application/json'},
+        body: JSON.stringify({email, password}),
+        credentials: "include",
+      })
+
+      if (!res.ok) {
+        const data = await res.json();
+        setError(data.message || "Login Failed")
+      } else {
+        router.push("/admin/dashboard")
+      }
+    } catch (err) {
+      setError("Something went wrong. :( ")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  onChange={(e) => setEmail(e.target.value)}
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input onChange={(e) => setPassword(e.target.value)} id="password" type="password" required />
+              </div>
+              {error && <p className="text-red-500 text-sm">{error}</p>}
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full" disabled={loading}>
+                  {loading ? "Logging in..." : "Login"}
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
